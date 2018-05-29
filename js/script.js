fbe9ec7ac9cf21fb4d0235d64c90baaf847bc017
Daniel Melonio <danielcmelonio@outlook.com.br>
2018-05-29 14:36:59 -0300

    Pronto

---
+++ js/script.js
@@ -0,0 +1,51 @@
+var paciente = document.querySelector('#primeiro-paciente');
+
+var tdPeso = paciente.querySelector('.info-peso');
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector('.info-altura');
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector('.info-imc');
+
+
+
+tdImc.textContent = imc;
+
+var pesoTrue = true;
+var alturaTrue = true;
+
+if(peso < 0 | peso >= 500)
+{
+    pesoTrue = false;
+    console.log("Peso inválido");
+}
+
+if(altura < 0 || altura > 3)
+{
+    pesoTrue = false;
+    tdImc.textContent = "Altura inválida";
+}
+
+if(pesoTrue && alturaTrue)
+{
+    var imc = peso/Math.pow(altura, 2);
+}
+
+/*
+var pac = document.querySelector('.paciente')
+
+for(var x = 1; x < 10; x++)
+{
+    var tdPeso = pac.querySelector('.info-peso');
+    var peso = tdPeso.textContent;
+    var tdAltura = pac.querySelector('.info-altura');
+    var altura = tdAltura.textContent;
+
+    var tdImc = pac.querySelector('.info-imc');
+
+    var imc = peso/Math.pow(altura, 2);
+
+    tdImc.textContent = imc;
+}
+*/
