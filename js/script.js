513d11998c7f9c0807656eef02389d2c15c2b36b
Daniel Melonio <danielcmelonio@outlook.com.br>
2018-05-29 15:33:30 -0300

    Alterações

--- js/script.js
+++ js/script.js
@@ -1,5 +1,46 @@
 var pacientes = document.querySelectorAll('.paciente');
 
+for(var i = 0; i < pacientes.length; i++)
+{
+    var paciente = pacientes[i];
+    var tdPeso = paciente.querySelector('.info-peso');
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector('.info-altura');
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector('.info-imc');
+
+    var pesoTrue = true;
+    var alturaTrue = true;
+
+    if(peso < 0 | peso >= 500)
+    {
+        pesoTrue = false;
+        console.log("Peso inválido");
+    }
+
+    if(altura < 0 || altura > 3)
+    {
+        pesoTrue = false;
+        tdImc.textContent = "Altura inválida";
+    }
+
+    if(pesoTrue && alturaTrue)
+    {
+        var imc = peso/Math.pow(altura, 2);
+        Math.round(tdImc.textContent = imc);
+    }
+
+    else
+    {
+        tdImc.textContent = "Valores inválidos!";
+    }
+}
+
+
+/*
+
 console.log(pacientes);
 
 var tdPeso = paciente.querySelector('.info-peso');
@@ -10,37 +51,6 @@ var altura = tdAltura.textContent;
 
 var tdImc = paciente.querySelector('.info-imc');
 
-
-
-tdImc.textContent = imc;
-
-var pesoTrue = true;
-var alturaTrue = true;
-
-if(peso < 0 | peso >= 500)
-{
-    pesoTrue = false;
-    console.log("Peso inválido");
-}
-
-if(altura < 0 || altura > 3)
-{
-    pesoTrue = false;
-    tdImc.textContent = "Altura inválida";
-}
-
-if(pesoTrue && alturaTrue)
-{
-    var imc = peso/Math.pow(altura, 2);
-    tdImc.textContent = imc;
-}
-
-else
-{
-    tdImc.textContent = "Valores inválidos!";
-}
-
-/*
 var pac = document.querySelector('.paciente')
 
 for(var x = 1; x < 10; x++)
